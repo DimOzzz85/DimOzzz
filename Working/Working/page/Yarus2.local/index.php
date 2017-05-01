a903ff9bfa42c2af732c2285cf79175be4ebe834
dmitriy <23fear@mail.ru>
2017-05-01 16:30:34 +0300

    all files

---
+++ Working/Working/page/Yarus2.local/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>ЦЕХ</title>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <div class="block block-50">
+                <a href="#" class="logo">ЦЕХ</a>
+                <ul>
+                    <li>Дизайн</li>
+                    <li>Одежда</li>
+                    <li>Имидж</li>
+                </ul>
+            </div>
+            <div class="block block-50">
+                <a href="#">+7(985)184-07-22</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="content">
+        <div class="wrapper">
+            <div class="block block-25">Свое<br/>производство</div>
+            <div class="block block-25">Быстро<br/>(от 2-ух дней)</div>
+            <div class="block block-25">Маленьткая<br/>предоплата</div>
+            <div class="block block-25">Маленькие<br/>и большие партии</div>
+        </div>
+    </div>
+</body>
+</html>
